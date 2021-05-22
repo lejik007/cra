167be37f72bba5a39bae746774ad637dc3700a25
Oleg Kravchenko <olukrav@mail.ru>
2021-05-22 22:47:26 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Title from './common/Title';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title />
+      </header>
+    </div>
+  );
+}
